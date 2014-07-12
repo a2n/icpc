545567e4511b8a024372186c8a1c27d3f42000f4
Alan Lu <a@28int.com>
2014-07-12 19:54:40 +0800

    Eliminated code.

--- 1000.c
+++ 1000.c
@@ -3,12 +3,8 @@
 
 int main (int argc, char* argv[])
 {
-    unsigned int a;
-    unsigned int b;
-
+    int a, b;
     scanf ("%d %d", &a, &b);
     printf ("%d\n", a + b);
-
     return EXIT_SUCCESS;
 }
-
